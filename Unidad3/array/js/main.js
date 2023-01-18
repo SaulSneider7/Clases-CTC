4b11cc97cdcc682de1c411fc23c851bee210ed1f
saulsneider7 <saulsneider@hotmail.com>
2023-01-18 10:01:08 -0500

    Ejercicio Completo

--- Unidad3/array/js/main.js
+++ Unidad3/array/js/main.js
@@ -7,7 +7,7 @@ const listaDeNumero = [3, 8, 17, 5, 1, 22, 13, 15, 20];
 
 // ***************************
 
-const lstDeNumero = [1, 3, 5, 7, 9];
+const lstDeNumero = [1, 3, 5, 7];
 let cantidad = lstDeNumero.length;
 
 for (i = 0; cantidad > i; i++){
@@ -20,12 +20,29 @@ console.log("siguiente ejercicio");
 // **************************
 
 const lista = [1, 2, 3, 4, 5];
+let cantidad2 = lista.length;
 
 lista [0] = 10;
 lista [1] = 'Chicas-Tec';
 lista [2] = true;
 lista [3] = -2;
 lista [4] = 10.5;
+
+for (i = 0; cantidad2 > i; i++){
+    console.log(lista[i]);
+}
+
+// *******************Array 2 Dimensiones
+
+const preguntas = 
+[
+    ['¿Cual es la capital de Peru?', 'lima'],
+    ['¿Que idioma hablas?', 'Español'],
+    ['¿Cual es tu edad?', 24]
+];
+
+console.log(preguntas[0][1]);
+
 
 
 
