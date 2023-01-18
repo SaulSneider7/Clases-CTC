43bf0966fc369eb292dea4a173e44d20258d6858
saulsneider7 <saulsneider@hotmail.com>
2023-01-18 09:27:50 -0500

    array ejercicio

---
+++ Unidad3/array/js/main.js
@@ -0,0 +1,35 @@
+const listaDeNumero = [3, 8, 17, 5, 1, 22, 13, 15, 20];
+
+// console.log(listaDeNumero[0]);
+// console.log(listaDeNumero[4]);
+// console.log(listaDeNumero[6]);
+// console.log(listaDeNumero[8]);
+
+// ***************************
+
+const lstDeNumero = [1, 3, 5, 7, 9];
+let cantidad = lstDeNumero.length;
+
+for (i = 0; cantidad > i; i++){
+    console.log(lstDeNumero[i]);
+}
+
+
+// *************************
+console.log("siguiente ejercicio");
+// **************************
+
+const lista = [1, 2, 3, 4, 5];
+
+lista [0] = 10;
+lista [1] = 'Chicas-Tec';
+lista [2] = true;
+lista [3] = -2;
+lista [4] = 10.5;
+
+
+
+// let nombre = 'Saul';
+// let cantidadNombre = nombre.length;
+// console.log(nombre);
+// console.log(cantidadNombre);
