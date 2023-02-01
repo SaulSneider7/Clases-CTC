e9f2a279e9e307f179139f8a89fc8f8afc199033
saulsneider7 <saulsneider@hotmail.com>
2023-02-01 11:28:09 -0500

    estructura-de-datos

--- Unidad3/estructuraDatos/main.js
+++ Unidad3/estructuraDatos/main.js
@@ -1,37 +1,95 @@
-//Ejemplo Pila
-class Pila{
-    //Creamos constructor
+// // =================================
+// //          Ejemplo Pila
+// // =================================
+// class Pila{
+//     //Creamos constructor
+//     constructor(){
+//         this.Pila = [];
+//     }
+
+//     //agregar un elemento al final del array
+//     push(item){
+//         this.Pila.push(item);
+//         return this.Pila;
+//     }
+//     //Metodo para eliminar elemento
+//     pop(){
+//         return this.Pila.pop();
+//     }
+//     //Metodo que retorne el ultimo valor ingresado
+//     ultimoValor(){
+//         return this.Pila[this.Pila.length -1];
+//     }
+//     //Metodo que retorna el cantidad de elementos del array
+//     longitud(){
+//         return this.Pila.length;
+//     }
+//     //Muestre el contenido del array en la consola
+//     mostrar(){
+//         console.log(this.Pila);
+//     }
+// }
+
+
+// let abc = new Pila();
+
+// console.log(abc);
+// console.log(abc.longitud());
+// console.log(abc.push('Naranja'));
+// console.log(abc.push('Manzana'));
+// console.log(abc.longitud());
+// abc.mostrar();
+// console.log(abc.ultimoValor());
+// abc.mostrar();
+// abc.pop();
+// abc.mostrar();
+// console.log(abc.ultimoValor());
+
+
+// =================================
+//          Ejemplo Cola
+// =================================
+
+class Cola {
     constructor(){
-        this.Pila = [];
+        this.cola = [];
     }
-    //agregar un elemento al final del array
-    push(item){
-        this.Pila.push(item);
-        return this.Pila;
+    agregar(valor){
+        this.cola.push(valor);
+        return this.cola;
     }
-    //Metodo para eliminar elemento
-    pop(){
-        return this.Pila.pop();
+    eliminar(){
+        return this.cola.pop();
     }
-    //Metodo que retorne el ultimo valor ingresado
-    ultimoValor(){
-        return this.Pila[this.Pila.length -1];
-    }
-    //Metodo que retorna el cantidad de elementos del array
     longitud(){
-        return this.Pila.length;
+        return this.cola.length;
     }
-    //Muestre el contenido del array en la consola
+    vacio(){
+        return this.cola.length == 0;
+    }
     mostrar(){
-        console.log(this.Pila);
+        console.log(this.cola);
     }
 }
 
+let prueba = new Cola();
+prueba.agregar('rojo');
+prueba.agregar('verde');
+prueba.agregar('azul');
+prueba.agregar('amarillo');
+prueba.agregar('negro');
+prueba.mostrar();
 
-let abc = new Pila();
-console.log(abc);
-console.log(abc.longitud());
-console.log(abc.push('Naranja'));
-console.log(abc.push('Manzana'));
-console.log(abc.longitud());
-abc.mostrar();
+prueba.eliminar();
+prueba.mostrar();
+
+console.log(prueba.longitud());
+console.log(prueba.vacio());
+
+
+// =================================
+//          Ejemplo Matrices
+// =================================
+
+let test = [20 ,'okay', 1, 2,""];
+
