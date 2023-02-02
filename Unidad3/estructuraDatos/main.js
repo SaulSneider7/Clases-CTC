936c3d2ac7b3053a7a7bd1fc9f3b003ba14e5946
saulsneider7 <saulsneider@hotmail.com>
2023-02-02 09:35:06 -0500

    Estructura de datos - fin

--- Unidad3/estructuraDatos/main.js
+++ Unidad3/estructuraDatos/main.js
@@ -50,46 +50,72 @@
 //          Ejemplo Cola
 // =================================
 
-class Cola {
-    constructor(){
-        this.cola = [];
-    }
-    agregar(valor){
-        this.cola.push(valor);
-        return this.cola;
-    }
-    eliminar(){
-        return this.cola.pop();
-    }
-    longitud(){
-        return this.cola.length;
-    }
-    vacio(){
-        return this.cola.length == 0;
-    }
-    mostrar(){
-        console.log(this.cola);
-    }
-}
+// class Cola {
+//     constructor(){
+//         this.cola = [];
+//     }
+//     agregar(valor){
+//         this.cola.push(valor);
+//         return this.cola;
+//     }
+//     eliminar(){
+//         return this.cola.pop();
+//     }
+//     longitud(){
+//         return this.cola.length;
+//     }
+//     vacio(){
+//         return this.cola.length == 0;
+//     }
+//     mostrar(){
+//         console.log(this.cola);
+//     }
+// }
 
-let prueba = new Cola();
-prueba.agregar('rojo');
-prueba.agregar('verde');
-prueba.agregar('azul');
-prueba.agregar('amarillo');
-prueba.agregar('negro');
-prueba.mostrar();
+// let prueba = new Cola();
+// prueba.agregar('rojo');
+// prueba.agregar('verde');
+// prueba.agregar('azul');
+// prueba.agregar('amarillo');
+// prueba.agregar('negro');
+// prueba.mostrar();
 
-prueba.eliminar();
-prueba.mostrar();
+// prueba.eliminar();
+// prueba.mostrar();
 
-console.log(prueba.longitud());
-console.log(prueba.vacio());
+// console.log(prueba.longitud());
+// console.log(prueba.vacio());
 
 
 // =================================
 //          Ejemplo Matrices
 // =================================
 
-let test = [20 ,'okay', 1, 2,""];
+// let martriz1 = [20 ,'okay', 1, 2,""];
 
+// martriz1.push('rojo');
+// martriz1.push(24);
+// console.log(martriz1);
+// martriz1.pop();
+// console.log(martriz1);
+
+// =================================
+//          Ejemplo Arbol
+// =================================
+class Arbol{
+    constructor (node) {
+        this._node = node;
+        this._array = [];
+    }
+}
+
+let a = new Arbol(1000000);
+let b = new Arbol(99);
+let c = new Arbol(100);
+let d = new Arbol(676);
+let e = new Arbol(2);
+
+a._array.push(b);
+b._array.push(c,d,e)
+console.log(a);
+console.log(b);
